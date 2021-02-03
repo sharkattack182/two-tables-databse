33a42d765ad2b67f34a8329d21fb6a4c47cd3889
Matthew Courtney <matthewdc182@gmail.com>
2021-02-02 19:14:29 -0500

    prompted the user using inquirer and created a switch statement to sort the answer

--- server.js
+++ server.js
@@ -13,4 +13,37 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connection complete")
-})
+});
+
+
+function search() {
+    inquirer.prompt({
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: ["Search by artist", "Search by song", "Find artists with top song and album", "Search Artists that appear more than once", "Find data within a specific range"],
+        name: "input"
+    }).then(function(response) {
+        switch (response.input) {
+            case "Search by artist":
+            artistSearch();
+            break;
+
+            case "Search by song":
+            songSearch();
+            break;
+
+            case "Find artists with top song and album":
+            songAndAlbumSearch();
+            break;
+
+            case "Search Artists that appear more than once":
+            multipleSearch();
+            break;
+
+            case "Find data within a specific range":
+            rangeSearch();
+            break;
+        }
+    })
+}
+
